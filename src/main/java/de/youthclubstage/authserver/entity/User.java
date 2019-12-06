64186fad794d04b8c50f6397a7e39f1a61635006
Sascha Deeg <sascha.deeg@gmail.com>
2019-12-07 00:58:09 +0100

    Fix Port #DeployDev

--- src/main/java/de/youthclubstage/authserver/entity/User.java
+++ src/main/java/de/youthclubstage/authserver/entity/User.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -23,7 +25,11 @@ import java.util.UUID;
 @Entity
 @Table
 public class User implements UserDetails {
+
     @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid",
+            strategy = "uuid")
     private UUID id;
 
     private String emailAddress;
